4ef30ed484635edc83cc7e6185ff971c9860b068
NFredericci <n204355@dac.unicamp.br>
2024-09-25 17:10:08 -0300

    Update test-reg_lin.R

--- tests/testthat/test-reg_lin.R
+++ tests/testthat/test-reg_lin.R
@@ -17,3 +17,37 @@ test_that("preditora de constantes", {
   data = data.frame(y, x1, x2)
   expect_error(reg_lin(data, "y", c("x1","x2")))
 })
+
+test_that("retorna uma lista", {
+  modelo = reg_lin(df, "y", c("x1", "x2"))
+  valores = matrix(c(7, 6), nrow = 1, byrow = TRUE)
+  expect_type(predicts(modelo, valores), "list")
+})
+
+test_that("resíduos nulos", {
+  x1 = c(13, 21, 33)
+  x2 = c(20, 37, 49)
+  y = 5 + 2*x1 + 3*x2
+  teste <- data.frame(y, x1, x2)
+  expect_equal(round(reg_lin(teste, "y", c("x1", "x2"))$preditos), y)
+})
+
+test_that("posto incompleto", {
+  x1 = c(1, 2, 3)
+  x2 = c(2, 4, 6)
+  y = c(20, 15, 1)
+  teste <- data.frame(y, x1, x2)
+  expect_error(reg_lin(teste, "y", c("x1", "x2")))
+})
+
+test_that("valores incompletos", {
+  modelo = reg_lin(df, "y", c("x1", "x2"))
+  valores = matrix(c(1, 5, 2), nrow = 3, byrow = TRUE)
+  expect_error(predicts(modelo, valores))
+})
+
+test_that("predição completa", {
+  modelo = reg_lin(df, "y", c("x1", "x2", "x3"))
+  valores = matrix(c(1, 5, 2, 6, 3, 7, 4, 9, 8), nrow = 3, byrow = TRUE)
+  expect_length(predicts(modelo, valores), ncol(valores))
+})
